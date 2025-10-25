9f5af1b4a9243a97c2dd2b6e1152377152abe528
Thoriqutsani Anastyo Rasnan <thoriq.rasnan@gmail.com>
2025-10-25 20:11:00 +0700

    add new function to get weather forecast based on hour

--- tools/weather.py
+++ tools/weather.py
@@ -45,7 +45,7 @@ def weather_tool(mcp):
 
     @mcp.tool()
     def get_forecast_weather(location: str = "Yogyakarta", days: int = 1) -> dict:
-        """Get forecast weather information for a city."""
+        """Get forecast weather information for a city in whole day."""
         if not location:
             return {"error": "Missing 'location' parameter."}
 
@@ -73,5 +73,41 @@ def weather_tool(mcp):
                 "condition": forecast_day["day"]["condition"]["text"],
                 "avg_temp_c": forecast_day["day"]["avgtemp_c"],
                 "air_quality": us_epa_standart[forecast_day["day"]["air_quality"]["us-epa-index"]],
+            })
+        return result
+
+    @mcp.tool()
+    def get_hour_forecast_weather(location: str, days: int, hour: int) -> dict:
+        """Get forecast weather information for a city in a specific hour (24 hour format)."""
+        if not location:
+            return {"error": "Missing 'location' parameter."}
+
+        if days < 0 or days > 14:
+            return {"error": "You can only see weather forecasts for 0–14 days."}
+
+        if hour < 0 or hour > 23:
+            return {"error": "A day can only consist of 24 hours."}
+
+        url = f"http://api.weatherapi.com/v1/forecast.json?key={WEATHER_API_KEY}&q={location}&days={days}&aqi=yes&hour={hour}"
+        try:
+            res = requests.get(url, timeout=10)
+            res.raise_for_status()
+            data = res.json()
+        except Exception as e:
+            return {"error": f"Failed to fetch data: {e}"}
+
+        # Handle API errors
+        if "error" in data:
+            return {"error": data["error"].get("message", "Unknown error")}
+
+        result = []
+        for i in range(days):
+            forecast_day = data["forecast"]["forecastday"][i]
+            result.append({
+                "city": data["location"]["name"],
+                "date": forecast_day["date"],
+                "condition": forecast_day["hour"][0]["condition"]["text"],
+                "temp_c": forecast_day["hour"][0]["temp_c"],
+                "air_quality": us_epa_standart[forecast_day["hour"][0]["air_quality"]["us-epa-index"]],
             })
         return result
